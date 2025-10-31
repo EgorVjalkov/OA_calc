2d328dd8eb431874ab0db3deb46ea7c76fa98627
JegorVjalkov <e.vjalkov@yandex.ru>
2025-11-01 00:14:48 +0300

    исправлен баг с bot.py

--- oac/dialogs/misc_dialogs/misc_selected.py
+++ oac/dialogs/misc_dialogs/misc_selected.py
@@ -1,8 +1,8 @@
 from aiogram.types import Message
+from aiogram import Bot
 from aiogram_dialog import DialogManager
 from aiogram_dialog.widgets.input.text import TextInput
 
-from oac.bot import my_bot
 from config_reader import config
 
 
@@ -12,7 +12,7 @@ async def on_click(m: Message,
                    input_data: str,
                    ** kwargs) -> None:
 
-    bot = my_bot.get_bot()
+    bot = Bot(config.get_token())
     text = f'@{m.from_user.username}:\n{input_data}'
     await dm.event.answer('Спасибо. Сообщение отправлено')
     await bot.send_message(config.get_admin_id(), text)
